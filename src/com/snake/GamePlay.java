61a552ce1fdaab46da0d6981dcdc76e756f326a2
denis <denisvitalievich5@gmail.com>
2019-12-30 22:57:21 +0200

    "Snake" can collect apple, and sout in console. If snake touches border, then game will end

--- src/com/snake/GamePlay.java
+++ src/com/snake/GamePlay.java
@@ -9,57 +9,95 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.util.Random;
 
 
 public class GamePlay extends JPanel implements ActionListener, KeyListener {
-    private Timer t = new Timer(5, this);
-    private double x = 0;
-    private double y = 0;
+    private Timer t = new Timer(3, this);
+    private double x = 40;
+    private double y = 40;
+    private double xA = 40;
+    private double yA = 40;
     private double velx = 0;
     private double vely = 0;
-    
+    private int count = 0;
 
-    public GamePlay() {
+    GamePlay() {
         t.start();
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
+
     }
 
     @Override
     protected void paintComponent(Graphics graphics) {
         super.paintComponent(graphics);
         Graphics2D g2 = (Graphics2D) graphics;
-        Ellipse2D ellipse = new Ellipse2D.Double(x,y,40,40);
+        Rectangle2D rectangle2D = new Rectangle2D.Double(x,y,15,15);
         g2.setPaint(Color.BLACK);
-        g2.fill(ellipse);
+        g2.fill(rectangle2D);
     }
+
+
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
-        repaint();
+        updateUI();
+        checkTouch();
         x += velx;
         y += vely;
     }
 
-    public void up(){
+    @Override
+    protected void paintChildren(Graphics graphics) {
+        super.paintChildren(graphics);
+        Graphics2D g2 = (Graphics2D) graphics;
+        double px = xA;
+        double py = yA;
+        if( (x < px+5) && (x > px-5) ){
+            if((y < py+5) && (y > py-5)) {
+                Random random = new Random();
+                xA = random.nextInt(601);
+                yA = random.nextInt(601);
+                count++;
+                System.out.println(count);
+            }
+        }
+        Rectangle2D apple = new Rectangle2D.Double(xA,yA,15,15);
+        g2.setPaint(Color.RED);
+        g2.fill(apple);
+    }
+
+
+    private void up(){
         vely = -0.5;
         velx = 0;
     }
 
-    public void down(){
+    private void down(){
         vely = 0.5;
         velx = 0;
     }
 
-    public void left(){
+    private void left(){
         vely = 0;
         velx = -0.5;
     }
-    public void right(){
+    private void right(){
         vely = 0;
         velx = 0.5;
     }
+
+    private void checkTouch(){
+        if ( (x >= 0 && x <= 600) && y == 0){ System.exit(0);}
+        if ( (y >= 0 && y <= 600) && x == 0 ){System.exit(0);}
+        if ( (x >= 0 && x <= 600) && y == 600 ){System.exit(0);}
+        if ( (y >= 0 && y <= 600) && x == 600 ){System.exit(0);}
+    }
+
+
     public void keyPressed(KeyEvent e){
         int code = e.getKeyCode();
         if(code == KeyEvent.VK_UP){
